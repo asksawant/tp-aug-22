c4f46399d543b70df3f721bd77afad9b1d30988d
asksawant <sprathameshsawant@gmail.com>
2022-08-19 03:23:38 +0530

    day 1 work

--- src/create_folds.py
+++ src/create_folds.py
@@ -1,31 +1,32 @@
 import pandas as pd
+import numpy as np
 from sklearn import model_selection
 
 if __name__ == "__main__":
 
     # Read the training data
-    df = pd.read_csv("input/train.csv")
+    df_train = pd.read_csv("input/train.csv")
+    print("Shape of the dataset: ",df_train.shape)
 
     # Creating the a new column called kfold and assigning the value -1
-    df["kfold"] = -1
-
+    df_train['kfold'] = -1
+    print("Shape of the dataset after adding kfold col: ",df_train.shape)
     # Shuffling the data
-    df = df.sample(frac=1).reset_index(drop=True)
+    # df = df.sample(frac=1).reset_index(drop=True)
 
     # Selecting the target variables
-    y = df.failure.values
+    y = df_train.failure.values
 
     # initiate the kfold class from model_selection module
     # as this is imbalance dataset
     # using StratifiedKfold
 
-    kf = model_selection.StratifiedGroupKFold(n_splits=5)
+    kf = model_selection.StratifiedGroupKFold(n_splits=5, shuffle=True, random_state=42)
 
     # fill the new kfold column
-    for f, (t_,v_) in enumerate(kf.split(X = df, y = y)):
-        print(len(t_),len(v_))
-        df.loc[v_, 'kfold'] = f
+    for fold, (train_indicies,valid_indicies) in enumerate(kf.split(X=df_train,y=y)):
+        print(len(train_indicies),len(valid_indicies))
+        df_train.loc[valid_indicies,'kfold'] = fold
     
     # save the new csv with kfold column
-    df.to_csv("input/train_folds.csv", index = False)
-    
+    df_train.to_csv("input/train_folds.csv", index = False)
